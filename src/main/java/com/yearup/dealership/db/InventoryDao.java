380e5909ff5a7f76a8b0c6c9ecc06167048544b7
Stephen_A <ankustephen99@gmail.com>
2025-12-09 06:39:10 -0500

    Added methods to InventoryDao.java allowing the class to add and remove vehicles from inventory database

--- src/main/java/com/yearup/dealership/db/InventoryDao.java
+++ src/main/java/com/yearup/dealership/db/InventoryDao.java
@@ -14,9 +14,26 @@ public class InventoryDao {
 
     public void addVehicleToInventory(String vin, int dealershipId) {
         // TODO: Implement the logic to add a vehicle to the inventory
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "INSERT INTO inventory (dealership_id, VIN) VALUES (?, ?)")) {
+            preparedStatement.setInt(1, dealershipId);
+            preparedStatement.setString(2, vin);
+            preparedStatement.executeUpdate();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     public void removeVehicleFromInventory(String vin) {
         // TODO: Implement the logic to remove a vehicle from the inventory
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "DELETE FROM inventory WHERE VIN = ?")) {
+            preparedStatement.setString(1, vin);
+            preparedStatement.executeUpdate();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
